7ce987f41e0f43ffabc3f0ac2a7d6cf07a792410
Dario Marranti <dario.marranti@gmail.com>
2026-03-27 17:40:02 -0300

    improve code

--- src/main/java/com/hotel/search/infrastructure/adapter/in/rest/validation/CheckInBeforeCheckOutValidator.java
+++ src/main/java/com/hotel/search/infrastructure/adapter/in/rest/validation/CheckInBeforeCheckOutValidator.java
@@ -4,12 +4,7 @@ import com.hotel.search.infrastructure.adapter.in.rest.dto.SearchRequestDTO;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-/**
- * Constraint validator that ensures {@code checkIn} is strictly before {@code checkOut}.
- * <p>
- * Null dates are not evaluated here; {@code @NotNull} on each field handles that case.
- * </p>
- */
+
 public class CheckInBeforeCheckOutValidator
         implements ConstraintValidator<CheckInBeforeCheckOut, SearchRequestDTO> {
 
